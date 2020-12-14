ebd9390af16a0c8e33264f3492c8b9de5eb596ac
ryan <ryan101789@gmail.com>
2020-12-14 14:04:16 +0900

    first

---
+++ src/main/java/com/example/demo/cop/bbs/service/model/ArticleDto.java
@@ -0,0 +1,11 @@
+package com.example.demo.cop.bbs.service.model;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Data
+@Component
+public class ArticleDto {
+
+}
---
+++ src/main/java/com/example/demo/sym/service/model/TeacherDto.java
@@ -0,0 +1,11 @@
+package com.example.demo.sym.service.model;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Component
+@Data
+public class TeacherDto {
+
+}
